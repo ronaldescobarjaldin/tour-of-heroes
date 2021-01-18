3a46e76bb9bda2726a2d634ab224159412374172
ronaldescobarjaldin <ronald.escobar@ucb.edu.bo>
2021-01-18 12:11:24 -0400

    finished hero editor and started to add hero list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,8 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero = 'Windstorm';
-  
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  heroes: Hero[] = HEROES;
+
   constructor() { }
 
   ngOnInit(): void {
